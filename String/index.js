855e1ea0194341e673456bc593e1716ba82b250f
Bach Phung <bach.phung@tuni.fi>
2021-11-26 21:43:02 +0200

    String

---
+++ String/index.js
@@ -0,0 +1,41 @@
+var myString = '   Learn JavaScript FullStack Course JavaScript'
+
+// 1:Length
+//console.log(myString.length);
+
+// 2:Find index
+// console.log(myString.indexOf('Java'));
+// console.log(myString.lastIndexOf('Java'));
+// console.log(myString.search('JavaScript'));
+
+// 3:Cut string
+// console.log(myString.slice(6,16));
+// console.log(myString.slice(-1,-11));
+
+// 4:Replace
+//console.log(myString.replace(/JavaScript/g, 'JS'));
+
+// 5:Convert to upper case
+//console.log(myString.toUpperCase());
+
+// 6:Convert to lower case
+//console.log(myString.toLowerCase());
+
+// 7:Trim
+//console.log(myString.trim());
+
+// 8:Split
+// var languages ='JavaScript, PHP, Ruby'
+// var language = 'JavaScript'
+// console.log(languages.split(', '));
+// console.log(language.split(''));
+
+// 9:Get a character by index
+// const myString2 = 'Bach Phung'
+// console.log(myString2.charAt(0));
+
+//10:Concat string
+// const string1 = 'Bach'
+// const string2 = 'Phung'
+// console.log(string1.concat(' ' + string2));
+
