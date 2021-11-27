fa9b21b52b20d34935e0ad1a4137f2c7e46db22d
Bach Phung <bach.phung@tuni.fi>
2021-11-27 18:19:36 +0200

    Array

---
+++ Array/index.js
@@ -0,0 +1,42 @@
+var languages = ['Javascript', 'PHP', 'Ruby']
+
+
+//1. To String
+//console.log(languages.toString());
+
+//2. Join
+// console.log(languages.join(', '));
+
+//3. Pop
+// languages.pop()
+//Delete the element at the end of array
+//and return a new array
+// console.log(languages);
+
+//4. Push
+// languages.push('Docker')
+// console.log(languages);
+
+//5. Shift
+// languages.shift()
+// console.log(languages);
+
+//6. Unshift
+// languages.unshift('Dart')
+// console.log(languages);
+
+//7. Splicing
+// languages.splice(1,0,'Dart')
+// console.log(languages);
+
+//8. Concat
+// var languages2 = [
+//     'Dart',
+//     'Ruby'
+// ]
+
+// console.log(languages.concat(languages2));
+
+//9. Slicing
+console.log(languages.slice(1,2));
+console.log(languages.slice(0));
