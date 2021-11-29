09779749d49fe01106feffce724d40a9e76f910b
Bach Phung <bach.phung@tuni.fi>
2021-11-29 19:47:20 +0200

    Math Object

---
+++ Math/index.js
@@ -0,0 +1,17 @@
+// 1: Math.PI
+// 2: Math.round()
+// 3: Math.abs()
+// 4: Math.ceil()
+// 5: Math.floor()
+// 6: Math.random()
+// 7: Math.min()
+// 8: Math.max()
+
+console.log(Math.PI);
+console.log(Math.round(1.5));
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.6));
+console.log(Math.floor(Math.random()*10));
+console.log(Math.min(12, 2312, 2, 5, 10));
+console.log(Math.max(12, 2312, 2, 5, 10));
